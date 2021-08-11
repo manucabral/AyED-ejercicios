b005c59b0e217cdedfb0387782f2ebc92648a4ac
Manu <mcabral@frba.utn.edu.ar>
2021-08-11 12:15:17 -0300

    add arrays: exercise 6
    order array elements with bubble sort (descending)

---
+++ Adicional/Vectores/E6.cpp
@@ -0,0 +1,34 @@
+/*
+    Realice un subprograma que muestre el contenido de un vector en forma 
+    ordenada descendentemente. El tamaño del array es informado por 
+    parámetro
+*/
+
+#include <iostream>
+
+using namespace std;
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubble_sort_desc(int *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+        for (int j = 0; j < size - 1; j++)
+            if (arr[j] < arr[j + 1])
+                swap(&arr[j], &arr[j+1]);
+
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+}
+
+int main(int argc, char const *argv[])
+{
+    int arr[6] = {10, 300, 85, 32, 12};
+    bubble_sort_desc(arr, 5);
+    return 0;
+}
